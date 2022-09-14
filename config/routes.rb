92d00312eebc57656dc16a376512eeb264bad424
Eryck Milano <eryckmilano@gmail.com>
2022-09-13 22:49:33 -0300

    routes

--- config/routes.rb
+++ config/routes.rb
@@ -3,8 +3,7 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
-  resources :restaurants do
-    resources :reviews, only: [ :new, :create]
+  resources :restaurants, only: [:index, :new, :create, :show ] do
+    resources :reviews, only: [:new, :create]
   end
-  root to: 'application#home'
 end
